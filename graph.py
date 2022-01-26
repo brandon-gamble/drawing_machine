b20a4532e19f59dbd3a50720d6b6a532e0bc981e
brandon <brandon05250@gmail.com>
2022-01-26 16:34:02 -0500

    graph.py: added comments

--- graph.py
+++ graph.py
@@ -11,61 +11,95 @@ import scratch
 def activation(v1,w1,v2,w2):
     return scratch.get_2ci_pos(v1,v2,w1,w2)
 
-#########
-# GRAPH #
-#########
-
-
-
-
-########
-# TREE #
-########
 class node:
+    '''
+    typical tree notation is that parent is root and each parent as two children.
+    however, for case of linkage solver, tree is traversed from leaves back up to root.
+    as we traverse, every node is the solution of two inputs. as such, we will use
+    the opposite of typical tree notation, instead saying that every child has exactly
+    two parents. to compute the child, we solve an activation function with the parents as inputs
+    '''
     def __init__(self, value):
+        # initalize Node
+        # for empty node, initialize as "node.(None)"
         self.value = value
-        self.left_child = None
-        self.left_weight = 1
-        self.right_child = None
+        self.left_parent = None  # initialize without data in parents
+        self.right_parent = None
+        self.left_weight = 1    # initailize with weight of 1
         self.right_weight = 1
 
     # def insert_left(self, value):
-    #     if self.left_child == None:
-    #         self.left_child = node(value)
+    #     if self.left_parent == None:
+    #         self.left_parent = node(value)
     #     else:
     #         new_node = node(value)
-    #         new_node.left_child = self.left_child
-    #         self.left_child = new_node
+    #         new_node.left_parent = self.left_parent
+    #         self.left_parent = new_node
     #
     # def insert_right(self, value):
-    #     if self.right_child == None:
-    #         self.right_child = node(value)
+    #     if self.right_parent == None:
+    #         self.right_parent = node(value)
     #     else:
     #         new_node = node(value)
-    #         new_node.right_child = self.right_child
-    #         self.right_child = new_node
+    #         new_node.right_parent = self.right_parent
+    #         self.right_parent = new_node
 
     def populate_node(self):
-        self.value = activation(self.left_child.value,self.left_weight, self.right_child.value,self.right_weight)
+        # populate node with activation function
+        self.value = activation(self.left_parent.value,self.left_weight, self.right_parent.value,self.right_weight)
 
 def preorder(root):
+    # print root values by preorder algorithm
     if root:
         print(root.value)
-        preorder(root.left_child)
-        preorder(root.right_child)
+        preorder(root.left_parent)
+        preorder(root.right_parent)
 
 def inorder(root):
+    # print root values by inorder algorithm
     if root:
-        inorder(root.left_child)
+        inorder(root.left_parent)
         print(root.value)
-        inorder(root.right_child)
+        inorder(root.right_parent)
+
 def postorder(root):
+    # print root values by postorder algorithm
+    # postorder, as it turns out, guarantees that we start with leaves and traverse
+    # up tree to root. this order is sufficient for populating tree data from inputs
+    # at leaves to output at root
     if root:
-        postorder(root.left_child)
-        postorder(root.right_child)
+        postorder(root.left_parent)
+        postorder(root.right_parent)
         print(root.value)
 
 def postorder_populate(root):
+    '''
+    our goal is to populate the tree starting with the leaves (input parent nodes)
+    and traversing all the way to the root. postorder traversal algorithm will give
+    us the correct order. at each node during the traversal, we need to check if it
+    contains data in its value attribute. if the value is None, then it has not been
+    populated and we must compute its value from its parents.
+
+    therefore, what we need is a flag for whether node.value is empty (None) or
+    full (contains an array).
+
+    if a node is empty, then "not node.value" returns True
+    if a node is full, then "not node.value" returns an error:
+        "truth value of array with more than one element is ambiguous"
+        suggestion is to use any(), but NoneType doesn't have any() attribute.
+
+    we are in a bit of a catch22.
+
+    to solve this, we put evaluation in a try/except
+    if there is no value, then the try block executes and is_empty is True
+    if there is a value, then the try block will fail and go to the except block,
+    where we mark the flag false.
+
+    finally, we start our recursive loop using "if is_empty: ..."
+
+    this loop is the same as the postorder algorithm but instead of printing
+    each node when we get to it, we populate it
+    '''
     try:
         # true if no value
         is_empty = not root.value
@@ -75,11 +109,40 @@ def postorder_populate(root):
         is_empty = False
 
     if is_empty: # if node is empty, enter loop to populate
-        postorder_populate(root.left_child)
-        postorder_populate(root.right_child)
+        postorder_populate(root.left_parent)
+        postorder_populate(root.right_parent)
         root.populate_node()
 
 def postorder_populate_w_clearance_check(root):
+    '''
+    our goal is to populate the tree starting with the leaves (input parent nodes)
+    and traversing all the way to the root. postorder traversal algorithm will give
+    us the correct order. at each node during the traversal, we need to check if it
+    contains data in its value attribute. if the value is None, then it has not been
+    populated and we must compute its value from its parents.
+
+    therefore, what we need is a flag for whether node.value is empty (None) or
+    full (contains an array).
+
+    if a node is empty, then "not node.value" returns True
+    if a node is full, then "not node.value" returns an error:
+        "truth value of array with more than one element is ambiguous"
+        suggestion is to use any(), but NoneType doesn't have any() attribute.
+
+    we are in a bit of a catch22.
+
+    to solve this, we put evaluation in a try/except
+    if there is no value, then the try block executes and is_empty is True
+    if there is a value, then the try block will fail and go to the except block,
+    where we mark the flag false.
+
+    finally, we start our recursive loop using "if is_empty: ..."
+
+    this loop is the same as the postorder algorithm but instead of printing
+    each node when we get to it, we populate it
+
+    *** before populating, we check to make sure we have clearance for the linkage
+    '''
     try:
         # true if no value
         is_empty = not root.value
@@ -89,20 +152,25 @@ def postorder_populate_w_clearance_check(root):
         is_empty = False
 
     if is_empty: # if node is empty, enter loop to populate
-        postorder_populate_w_clearance_check(root.left_child)
-        postorder_populate_w_clearance_check(root.right_child)
+        postorder_populate_w_clearance_check(root.left_parent)
+        postorder_populate_w_clearance_check(root.right_parent)
 
         # make bounding circles for traces of parent nodes
         # circle format is list with (x,y,r)
-        circ_left = smallestenclosingcircle.make_circle(root.left_child.value.transpose())
-        circ_right = smallestenclosingcircle.make_circle(root.right_child.value.transpose())
+        circ_left = smallestenclosingcircle.make_circle(root.left_parent.value.transpose())
+        circ_right = smallestenclosingcircle.make_circle(root.right_parent.value.transpose())
 
+        # distance between parent circles
         d = np.sqrt((circ_left[0]-circ_right[0])**2+(circ_left[0]-circ_right[0])**2)
 
+        # clerance of linkage
         clearance = (root.left_weight + root.right_weight) - (circ_left[2] + d + circ_right[2])
         if clearance >= 0:
+            # if we have positive clearance, populate the node
             root.populate_node()
         else:
+            # if we don't have clearance, then linkage would break if we tried to
+            # make a physical analog. print a warning
             print('clearance is %f. not long enough' % clearance)
 
 
@@ -157,19 +225,19 @@ if __name__ == "__main__":
         e = node(1)
         f = node(4)
 
-        a.left_child = c
+        a.left_parent = c
         a.left_weight = 3
-        a.right_child = b
+        a.right_parent = b
         a.right_weight = 1
 
-        b.left_child = c
+        b.left_parent = c
         b.left_weight = 2
-        b.right_child = d
+        b.right_parent = d
         b.right_weight = 1
 
-        d.left_child = e
+        d.left_parent = e
         d.left_weight = 1
-        d.right_child = f
+        d.right_parent = f
         d.right_weight = 0.5
 
         d.populate_node()
@@ -185,6 +253,7 @@ if __name__ == "__main__":
         print('a: %d' % a.value) # 13
         print('b: %d' % b.value) # 7
         print('d: %d' % d.value) # 3
+
 
 
     elif test == 2:
@@ -203,20 +272,20 @@ if __name__ == "__main__":
         l = node(12)
         m = node(13)
 
-        a.left_child = c
-        a.right_child = b
-        b.left_child = c
-        b.right_child = d
-        c.left_child = e
-        c.right_child = f
-        d.left_child = h
-        d.right_child = g
-        g.left_child = h
-        g.right_child = i
-        h.left_child = j
-        h.right_child = k
-        j.left_child = l
-        j.right_child = m
+        a.left_parent = c
+        a.right_parent = b
+        b.left_parent = c
+        b.right_parent = d
+        c.left_parent = e
+        c.right_parent = f
+        d.left_parent = h
+        d.right_parent = g
+        g.left_parent = h
+        g.right_parent = i
+        h.left_parent = j
+        h.right_parent = k
+        j.left_parent = l
+        j.right_parent = m
 
         print('preorder')
         preorder(a)
@@ -246,20 +315,20 @@ if __name__ == "__main__":
         h = node(None)
         j = node(None)
 
-        a.left_child = c
-        a.right_child = b
-        b.left_child = c
-        b.right_child = d
-        c.left_child = e
-        c.right_child = f
-        d.left_child = h
-        d.right_child = g
-        g.left_child = h
-        g.right_child = i
-        h.left_child = j
-        h.right_child = k
-        j.left_child = l
-        j.right_child = m
+        a.left_parent = c
+        a.right_parent = b
+        b.left_parent = c
+        b.right_parent = d
+        c.left_parent = e
+        c.right_parent = f
+        d.left_parent = h
+        d.right_parent = g
+        g.left_parent = h
+        g.right_parent = i
+        h.left_parent = j
+        h.right_parent = k
+        j.left_parent = l
+        j.right_parent = m
 
         print('postorder (unpopulated):')
         postorder(a)
@@ -274,7 +343,3 @@ if __name__ == "__main__":
         # 5,6,11, 5,6,11, 1,2,3, 3,6,1, 2,3,3, 6,4,10 ,16,27,38
         # which corresponds to:
         # e,f,c, e,f,c, l,m,j, k,h,l ,m,j,k, h,i,g, d,b,a
-
-#################################################
-#               solve using graph               #
-#################################################

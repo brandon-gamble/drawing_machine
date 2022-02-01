e4c221de0c0606500f66218dcacd724f60388d5d
brandon <brandon05250@gmail.com>
2022-02-01 13:54:09 -0500

    sandbox.py: added test case for a pantograph to enlarge a circle. encounters negative square root but still produces desired image.

--- sandbox.py
+++ sandbox.py
@@ -5,32 +5,111 @@ from matplotlib import pyplot as plt
 
 '''
 1   integrating graph and scratch to solve a node encoded 5bl
+2   using graph to solve pantograph
 
 '''
 
-test = 1
 
+test = 2
 
 if test == 1:
 
     n = 2000
-    c = np.array([[0],[0]])
-    drive1 = scratch.get_drive_trace(2,c,.12,n)
+    c = np.array([[0],[0]]) # circle center
+    drive1 = scratch.get_drive_trace(2,c,.12,n) # circular drive trace
 
-    c = np.array([[9],[0]])
-    drive2 = scratch.get_drive_trace(3,c,.11,n)
+    c = np.array([[9],[0]]) # circle center
+    drive2 = scratch.get_drive_trace(3,c,.11,n) # circular drive trace
 
-    a = graph.node(None) # root node
-    b = graph.node(drive1)
-    c = graph.node(drive2)
+    a = graph.node(None) # root node (spiro end effector)
+    b = graph.node(drive1) # driving node 1
+    c = graph.node(drive2) # drivign node 2
 
-    a.left_child = b
+    # connect a to b with link length 10
+    a.left_parent = b
     a.left_weight = 10
 
-    a.right_child = c
+    # connect a to c with link length 10
+    a.right_parent = c
     a.right_weight = 10
 
+    # compute end effector location
     graph.postorder_populate_w_clearance_check(a)
 
+    # plot
     plt.plot(a.value[0,:],a.value[1,:])
+    plt.show()
+
+elif test == 2:
+    '''
+    gets a math error ( invalid value in sqrt) in scratch.py line 61:
+    A = 1/4*((d+r1+r2)*(d+r1-r2)*(d-r1+r2)*(-d+r1+r2))**0.5
+
+    but well produces the pantograph effect of enlarging the pre-image
+
+    '''
+
+    #####################
+    # setup input nodes #
+    #####################
+
+    n = 600
+
+    fixed_pt = np.zeros([2,n]) # fixed point of pantograph
+
+    c = np.array([[.5],[0]]) # center of circle
+    pre_image = scratch.drive_trace.circle(0.1,c,.02,n) # circle as pre-image
+
+    #####################
+    # instantiate nodes #
+    #####################
+
+    a = graph.node(None)
+    b = graph.node(None)
+    c = graph.node(None)
+    d = graph.node(pre_image)
+    e = graph.node(None)
+    f = graph.node(fixed_pt)
+
+    ###########################
+    # build graph connections #
+    ###########################
+
+    a.left_parent = c
+    a.left_weight = 2
+    a.right_parent = b
+    a.right_weight = 1
+
+    b.left_parent = c
+    b.left_weight = 1
+    b.right_parent = d
+    b.right_weight = 1
+
+    c.left_parent = f
+    c.left_weight = 2
+    c.right_parent = e
+    c.right_weight = 1
+
+    e.left_parent = f
+    e.left_weight = 1
+    e.right_parent = d
+    e.right_weight = 1
+
+    ##################
+    # populate graph #
+    ##################
+    graph.postorder_populates(a)
+
+    ################
+    #     plot     #
+    ################
+
+    plt.plot(f.value[0,0],f.value[1,0]) # fixed point
+    plt.plot(d.value[0,:],d.value[1,:]) # pre-image
+
+    plt.plot(e.value[0,:],e.value[1,:])
+    plt.plot(c.value[0,:],c.value[1,:])
+    plt.plot(b.value[0,:],b.value[1,:])
+    plt.plot(a.value[0,:],a.value[1,:]) # image
+
     plt.show()
